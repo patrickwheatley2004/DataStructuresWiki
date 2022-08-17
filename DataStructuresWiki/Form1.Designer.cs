fd376e883b7a3b7f7245136f7ae91e775f897dd0
patrickwheatley2004 <patrickwheatley2004@gmail.com>
2022-08-17 10:31:26 +0800

    Created.

---
+++ DataStructuresWiki/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace DataStructuresWiki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvDataStructures = new System.Windows.Forms.ListView();
+            this.dataName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dataCategory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnDisplayInformation = new System.Windows.Forms.Button();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxCategory = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxStructure = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbxDefinition = new System.Windows.Forms.TextBox();
+            this.tbxSearchBar = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvDataStructures
+            // 
+            this.lvDataStructures.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.dataName,
+            this.dataCategory});
+            this.lvDataStructures.HideSelection = false;
+            this.lvDataStructures.Location = new System.Drawing.Point(130, 42);
+            this.lvDataStructures.Margin = new System.Windows.Forms.Padding(2);
+            this.lvDataStructures.MultiSelect = false;
+            this.lvDataStructures.Name = "lvDataStructures";
+            this.lvDataStructures.Size = new System.Drawing.Size(232, 337);
+            this.lvDataStructures.TabIndex = 0;
+            this.lvDataStructures.UseCompatibleStateImageBehavior = false;
+            this.lvDataStructures.View = System.Windows.Forms.View.Details;
+            this.lvDataStructures.SelectedIndexChanged += new System.EventHandler(this.lvDataStructures_SelectedIndexChanged);
+            // 
+            // dataName
+            // 
+            this.dataName.Text = "Name";
+            this.dataName.Width = 171;
+            // 
+            // dataCategory
+            // 
+            this.dataCategory.Text = "Category";
+            this.dataCategory.Width = 132;
+            // 
+            // btnDisplayInformation
+            // 
+            this.btnDisplayInformation.Location = new System.Drawing.Point(9, 351);
+            this.btnDisplayInformation.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDisplayInformation.Name = "btnDisplayInformation";
+            this.btnDisplayInformation.Size = new System.Drawing.Size(74, 28);
+            this.btnDisplayInformation.TabIndex = 1;
+            this.btnDisplayInformation.Text = "Display";
+            this.btnDisplayInformation.UseVisualStyleBackColor = true;
+            this.btnDisplayInformation.Click += new System.EventHandler(this.btnDisplayInformation_Click);
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(9, 20);
+            this.tbxName.Margin = new System.Windows.Forms.Padding(2);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(118, 20);
+            this.tbxName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 4);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Category";
+            // 
+            // tbxCategory
+            // 
+            this.tbxCategory.Location = new System.Drawing.Point(9, 55);
+            this.tbxCategory.Margin = new System.Windows.Forms.Padding(2);
+            this.tbxCategory.Name = "tbxCategory";
+            this.tbxCategory.Size = new System.Drawing.Size(118, 20);
+            this.tbxCategory.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 76);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Structure";
+            // 
+            // tbxStructure
+            // 
+            this.tbxStructure.Location = new System.Drawing.Point(9, 91);
+            this.tbxStructure.Margin = new System.Windows.Forms.Padding(2);
+            this.tbxStructure.Name = "tbxStructure";
+            this.tbxStructure.Size = new System.Drawing.Size(118, 20);
+            this.tbxStructure.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 111);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Definition";
+            // 
+            // tbxDefinition
+            // 
+            this.tbxDefinition.Location = new System.Drawing.Point(9, 127);
+            this.tbxDefinition.Margin = new System.Windows.Forms.Padding(2);
+            this.tbxDefinition.Multiline = true;
+            this.tbxDefinition.Name = "tbxDefinition";
+            this.tbxDefinition.Size = new System.Drawing.Size(118, 102);
+            this.tbxDefinition.TabIndex = 8;
+            // 
+            // tbxSearchBar
+            // 
+            this.tbxSearchBar.Location = new System.Drawing.Point(130, 20);
+            this.tbxSearchBar.Margin = new System.Windows.Forms.Padding(2);
+            this.tbxSearchBar.Name = "tbxSearchBar";
+            this.tbxSearchBar.Size = new System.Drawing.Size(154, 20);
+            this.tbxSearchBar.TabIndex = 10;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(288, 12);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(74, 28);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 434);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(600, 22);
+            this.statusStrip1.TabIndex = 12;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(9, 319);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(74, 28);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(11, 255);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(74, 28);
+            this.btnEdit.TabIndex = 14;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(11, 287);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(74, 28);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 456);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbxSearchBar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxDefinition);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxStructure);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxCategory);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.btnDisplayInformation);
+            this.Controls.Add(this.lvDataStructures);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvDataStructures;
+        private System.Windows.Forms.ColumnHeader dataName;
+        private System.Windows.Forms.ColumnHeader dataCategory;
+        private System.Windows.Forms.Button btnDisplayInformation;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxStructure;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbxDefinition;
+        private System.Windows.Forms.TextBox tbxSearchBar;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ DataStructuresWiki/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Patrick Wheatley
+// 11/08/2022
+namespace DataStructuresWiki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //9.1	Create a global 2D string array, use static variables for the dimensions (row, column)
+        static int dsRows = 12;
+        static int dsCols = 4;
+        string[,] DataStructures = new string[dsRows, dsCols];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDataStructuresInformation();
+        }
+
+        private void LoadDataStructuresInformation()
+        {
+            DataStructures[0, 0] = "Array";
+            DataStructures[0, 1] = "Array";
+            DataStructures[0, 2] = "Linear";
+            DataStructures[0, 3] = "Definition";
+
+            DataStructures[1, 0] = "Two Dimensional Array";
+            DataStructures[1, 1] = "Array";
+            DataStructures[1, 2] = "Linear";
+            DataStructures[1, 3] = "Definition";
+
+            DataStructures[2, 0] = "List";
+            DataStructures[2, 1] = "List";
+            DataStructures[2, 2] = "Linear";
+            DataStructures[2, 3] = "Definition";
+
+            DataStructures[3, 0] = "Linked list";
+            DataStructures[3, 1] = "List";
+            DataStructures[3, 2] = "Linear";
+            DataStructures[3, 3] = "Definition";
+
+            DataStructures[4, 0] = "Self-Balance Tree";
+            DataStructures[4, 1] = "Tree";
+            DataStructures[4, 2] = "Non-Linear";
+            DataStructures[4, 3] = "Definition";
+
+            DataStructures[5, 0] = "Heap";
+            DataStructures[5, 1] = "Tree";
+            DataStructures[5, 2] = "Non-Linear";
+            DataStructures[5, 3] = "Definition";
+
+            DataStructures[6, 0] = "Binary Search Tree";
+            DataStructures[6, 1] = "Tree";
+            DataStructures[6, 2] = "Non-Linear";
+            DataStructures[6, 3] = "Definition";
+
+            DataStructures[7, 0] = "Graph";
+            DataStructures[7, 1] = "Graphs";
+            DataStructures[7, 2] = "Non-Linear";
+            DataStructures[7, 3] = "Definition";
+
+            DataStructures[8, 0] = "Set";
+            DataStructures[8, 1] = "Abstract";
+            DataStructures[8, 2] = "Non-Linear";
+            DataStructures[8, 3] = "Definition";
+
+            DataStructures[9, 0] = "Queue";
+            DataStructures[9, 1] = "Abstract";
+            DataStructures[9, 2] = "Linear";
+            DataStructures[9, 3] = "Defnition";
+
+            DataStructures[10, 0] = "Stack";
+            DataStructures[10, 1] = "Abstract";
+            DataStructures[10, 2] = "Linear";
+            DataStructures[10, 3] = "Defnition";
+
+            DataStructures[11, 0] = "Hash Tabe";
+            DataStructures[11, 1] = "Hash";
+            DataStructures[11, 2] = "Non-Linear";
+            DataStructures[11, 3] = "Definition";
+        }
+
+        private void btnDisplayInformation_Click(object sender, EventArgs e)
+        {
+            // 9.8	Create a display method that will show the following information in a ListView: Name and Category
+            lvDataStructures.Items.Clear();
+            for (int i = 0; i < dsRows; i++)
+            {
+                ListViewItem lv1 = new ListViewItem(DataStructures[i,0],0);
+                lv1.SubItems.Add(DataStructures[i, 1]);
+                lvDataStructures.Items.Add(lv1);
+            }
+        }
+
+        private void displayTBXInformation(int slIndex)
+        {
+            tbxName.Text = DataStructures[slIndex,0];
+            tbxCategory.Text = DataStructures[slIndex,1];
+            tbxStructure.Text = DataStructures[slIndex,2];
+            tbxDefinition.Text = DataStructures[slIndex,3];
+        }
+
+        private void lvDataStructures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                displayTBXInformation(lvDataStructures.SelectedIndices[0]);
+            }
+            catch (Exception)
+            {
+
+            }          
+        }
+
+        private void updateSS(string input)
+        {
+            statusStrip1.Items.Clear();
+            statusStrip1.Items.Add(input);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string userText = tbxSearchBar.Text;
+        }
+
+        private void clearButtons()
+        {
+            // 9.5	Create a CLEAR method to clear the four text boxes so a new definition can be added
+            tbxName.Clear();
+            tbxCategory.Clear();
+            tbxStructure.Clear();
+            tbxDefinition.Clear();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearButtons();            
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            // 9.3	Create an EDIT button that will allow the user to modify any information from the 4 text boxes into the 2D array,
+            try
+            {
+                int index = lvDataStructures.SelectedIndices[0];
+                if (index >= 0 && index < dsRows)
+                {
+                    try
+                    {
+                        DataStructures[index, 0] = tbxName.Text;
+                        DataStructures[index, 1] = tbxCategory.Text;
+                        DataStructures[index, 2] = tbxStructure.Text;
+                        DataStructures[index, 3] = tbxDefinition.Text;
+                        updateSS("Data successfully edited.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Data unsuccessfully edited\nError Message: " + ex);
+                        updateSS("Data unsuccessfully edited\nError Message: " + ex);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error, nothing selected to edit.");
+                updateSS("Error, nothing selected to edit.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // 9.4	Create a DELETE button that removes all the information from a single entry of the array; the user must be prompted before the final deletion occurs, 
+            try
+            {
+                int index = lvDataStructures.SelectedIndices[0];
+                DialogResult dr = MessageBox.Show("Are you sure you want to delete " + DataStructures[index, 0] + "?", "Are you sure you want to delete?", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)
+                {
+                    DataStructures[index, 0] = null;
+                    DataStructures[index, 1] = null;
+                    DataStructures[index, 2] = null;
+                    DataStructures[index, 3] = null;
+                    updateSS(DataStructures[index, 0] + " was deleted.");
+                }
+                else if (dr == DialogResult.No)
+                {
+                    updateSS(DataStructures[index, 0] + " wasn't deleted.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error, nothing selected to delete.");
+                updateSS("Error, nothing selected to delete.");
+            }
+            
+        }
+    }
+}
